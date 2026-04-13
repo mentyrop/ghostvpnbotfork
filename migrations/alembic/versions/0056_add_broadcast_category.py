0573cbe65767b1fea2f198022e824f9f9b99379e
Nikita Malkov <NikiMalkov2@gmail.com>
2026-04-13 22:50:51 +0300

    ixed robokassa

--- migrations/alembic/versions/0056_add_broadcast_category.py
+++ migrations/alembic/versions/0056_add_broadcast_category.py
@@ -1,9 +1,10 @@
 """add broadcast category column
 
-Revision ID: 0054
-Revises: 0053
+Revision ID: 0056
+Revises: 0054
 Create Date: 2026-04-10
 
+(Chained after fork migration 0054 robokassa_payments; avoids duplicate 0054 id with upstream.)
 """
 
 from typing import Sequence, Union
@@ -11,8 +12,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = '0054'
-down_revision: Union[str, None] = '0053'
+revision: str = '0056'
+down_revision: Union[str, None] = '0054'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- migrations/alembic/versions/0057_add_pending_campaign_slug.py
+++ migrations/alembic/versions/0057_add_pending_campaign_slug.py
@@ -1,7 +1,7 @@
 """add pending_campaign_slug to users
 
-Revision ID: 0055
-Revises: 0054
+Revision ID: 0057
+Revises: 0056
 Create Date: 2026-04-13
 
 """
@@ -11,8 +11,8 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = '0055'
-down_revision: Union[str, None] = '0054'
+revision: str = '0057'
+down_revision: Union[str, None] = '0056'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
